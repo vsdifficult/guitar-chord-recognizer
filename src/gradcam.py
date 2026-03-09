4c5e94f67a7f7d3f1ac5fed504190a6acbf25fb4
vsdifficult <newfakes46578@gmail.com>
2026-03-09 11:51:55 +0300

    remove files

--- src/gradcam.py
+++
@@ -1,6 +0,0 @@
-from interpretability.gradcam import save_overlay
-
-
-if __name__ == "__main__":
-    out = save_overlay("models/guitar_chord_model.keras", "sample.jpg", method="gradcam")
-    print(out)
--- src/inference.py
+++
@@ -1,6 +0,0 @@
-from inference.predict import predict
-
-
-if __name__ == "__main__":
-    chord, conf, _ = predict("sample.jpg")
-    print(chord, conf)
